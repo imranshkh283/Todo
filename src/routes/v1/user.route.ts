aeac16f244623d4f91c7ab2365fec7acae9d60e3
Imran <imranshkh283@gmail.com>
2023-05-26 14:37:52 +0530

    route users

--- src/routes/v1/user.route.ts
+++ src/routes/v1/user.route.ts
@@ -4,6 +4,6 @@ import { userController, userValidation } from "../../modules/users";
 const router: Router = express.Router();
 
 router.route("/");
-router.route("/signup");
+router.post("/signup", userController.createUser);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import connectDB from "../config/database";
+import routes from "./routes/v1/";
 const app = express();
 
 connectDB();
 app.use(express.json());
-app.get("/", (_req, res) => {
+app.get("/", (req, res) => {
   res.send("API Running");
 });
+
+// v1 api routes
+app.use("/v1", routes);
 
 const server = app.listen(3000, () =>
   console.log(`Server started on port 3000`)
