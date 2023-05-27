d46ab533eea477c8cf20310f82c61cb0e4f7fddb
Imran <imranshkh283@gmail.com>
2023-05-27 21:17:49 +0530

    Done

--- dist/modules/token/token.type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    ACCESS: "access",
-    REFRESH: "refresh",
-    RESET_PASSWORD: "resetPassword",
-    VERIFY_EMAIL: "verifyEmail",
-};
--- dist/modules/users/users.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserGenderEnum = void 0;
-var UserGenderEnum;
-(function (UserGenderEnum) {
-    UserGenderEnum["male"] = "male";
-    UserGenderEnum["female"] = "female";
-    UserGenderEnum["others"] = "others";
-})(UserGenderEnum = exports.UserGenderEnum || (exports.UserGenderEnum = {}));
